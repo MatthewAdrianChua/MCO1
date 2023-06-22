edbb53be942382d4aa148acf45536ccd31b78221
Michael Joseph Go <135100340+wokest-boi@users.noreply.github.com>
2023-06-22 17:44:21 +0800

    DropBox works :)

--- script.js
+++ script.js
@@ -1,22 +1,7 @@
-const wrapper = document.querySelector('.wrapper');
-const loginLink = document.querySelector('.login-link');
-const registerLink = document.querySelector('.register-link');
-const dropDown = document.querySelector('#profilePic');
-const wrapperBtnClose = document.querySelector('.closelogreg');
-
-registerLink.addEventListener('click', () => {
-    wrapper.classList.add('active')
-});
-
-dropDown.addEventListener('click', () => {
-    document.getElementById('myDropdown').classList.toggle('show');
-});
+const profileBtn = document.querySelector('.profileBtn');
+const dropdownContent = document.querySelector('.dropdown');
 
 
-btnPopup.addEventListener('click', ()=> {
-    wrapper.classList.add('active-popup');
-});
-
-wrapperBtnClose.addEventListener('click', ()=> {
-    wrapper.classList.remove('active-popup');
-});
+profileBtn.addEventListener('click', e => {
+    dropdownContent.classList.toggle('show-menu');
+}); 
