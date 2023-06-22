8e6ad994ba0e0f56033ad59b07c67e011693b197
Zerithe <jonathandevera08@gmail.com>
2023-06-23 01:30:59 +0800

    added design and fixed layouting

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ const loginLink = document.querySelector('.login-link');
 const registerLink = document.querySelector('.register-link');
 const btnPopup = document.querySelector('#logregbtn');
 const wrapperBtnClose = document.querySelector('.closelogreg');
+const postPopup = document.querySelector('#createpost');
+const postThread = document.getElementById('#post-thread');
+let logStatus = true;
 
 registerLink.addEventListener('click', () => {
     wrapper.classList.add('active')
@@ -18,4 +21,25 @@ btnPopup.addEventListener('click', ()=> {
 
 wrapperBtnClose.addEventListener('click', ()=> {
     wrapper.classList.remove('active-popup');
+});
+
+postPopup.addEventListener('click', ()=> {
+    if(logStatus){
+        const post = `
+        <a class="post-button" href="#">
+            <div class="post-wrapper">
+                <ion-icon name="caret-up-circle-outline"></ion-icon>
+                <p class="post-numbers" id="">0</p>
+                <ion-icon name="caret-up-circle-outline"></ion-icon>
+                <p class="post-numbers">0</p>
+                <ion-icon name="chatbubble-ellipses-outline"></ion-icon>
+                <p class="post-numbers">0</p>
+                <p class="post-title">Lorem Ipsum Something Something</p>
+                <p class="post-author">By:Lorem Ipsum</p>
+            </div>
+        </a>`;
+        postThread.innerHTML += post;
+    }else{
+        wrapper.classList.add('active-popup');
+    }
 });
